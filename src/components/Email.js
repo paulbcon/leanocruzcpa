0bf0dd587404dedb0b938fb3b964a42f8fc4a32a
Paul Contemplacion <paulbcon@gmail.com>
2022-03-07 13:06:40 -0500

    Change the contact form

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,89 +1,59 @@
-import React,{useState,createRef} from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import EmailBg from '../assets/images/mailbox.jpg'
-import ReCAPTCHA from 'react-google-recaptcha'
-import { navigate } from 'gatsby'
-
+import { NetlifyForm, Honeypot } from "react-netlify-forms"
+import { Button } from 'react-bootstrap'
 
 const Email = () => {
-  const [state, setState] = useState({})
-  const recaptchaRef = createRef()
-  const [buttonDisabled, setButtonDisabled]  = useState(true)
-  
-  const handleChange = e => {
-      setState({...state, [e.target.name]: e.target.value})
-  }
+    
+    return (
+        <>
+            <EmailContainer>
+                <EmailContent>
+                    <h1>For inquiries</h1>
+                    <p>Please leave your email and message here:</p>
+                    <NetlifyForm
+                        name='contact'
+                        action='/thank-you'
+                        honeypotName='bot-field'
+                        onSuccess={(response, context) => {
+                            console.log('Successfully sent form data to Netlify Server')
+                            context.formRef.current.reset()
+                        }}
+                    >
+                        {({ handleChange, success, error }) => (
+                            <>
+                                <Honeypot />
+                                {success && (
+                                    <p>Thanks for contacting us!</p>
+                                )}
 
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) =>
-          encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
+                                {error && (
+                                    <p>
+                                        Sorry, we could not reach servers. Please contact (818) 378-8245 to learn more about our services. Thank you.
+                                    </p>
+                                )}
+                                <FormWrap>
+                                    <label htmlFor="yourname">Your Name:</label>
+                                    <input type="text" placeholder='Your Name' id="yourname" name="yourname" onChange={handleChange} aria-required="true" />
+                                    <label htmlFor='email'>Email:</label>
+                                    <input type="email" placeholder="Enter your email" id="email" name="email" onChange={handleChange} aria-required="true" />
+                                    <label htmlFor="message">Message:</label>
+                                    <textarea id="message" name="message" rows={5} cols={5} onChange={handleChange} aria-required="true" ></textarea>
 
-  const handleSubmit = e => {
-      e.preventDefault()
-      const form = e.target
-      const recaptchaValue = recaptchaRef.current.getValue()
+                                    <ButtonWrap>
+                                        <Button variant="primary" type="submit">Submit</Button>{' '};
+                                        <Button variant="warning" type="reset">Reset</Button>
+                                    </ButtonWrap>
+                                </FormWrap>
+                            </>
+                        )}
 
-      fetch('/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: encode({
-            'form-name': form.getAttribute('name'),
-            'g-recaptcha-response': recaptchaValue,
-            ...state,
-            }),
-      })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch(error => alert(error))
-  }
-  return (
-    <>
-     <EmailContainer>
-         <EmailContent>
-             <h1>For inquiries</h1>
-             <p>Please leave your email and message here:</p>
-             <form 
-                   name="contactform"
-                   method="POST" 
-                   data-netlify="true"
-                   netlify-honeypot="bot-field"
-                   data-netlify-recaptcha="true"
-                   action="/thank-you"
-                   onSubmit={handleSubmit}
-             >
-             <noscript>
-                <p>This form won’t work with Javascript disabled</p>
-            </noscript>    
-
-                 <input type="hidden" name="bot-field" />      
-                 <input type="hidden" name="form-name" value="contactform" />      
-                 <FormWrap>
-                     <label htmlFor="yourname">Your Name:</label>
-                     <input type="text" placeholder='Your Name' id="yourname" name="yourname" onChange={handleChange} aria-required="true" />
-                     <label htmlFor='email'>Email:</label>
-                     <input type="email" placeholder="Enter your email" id="email" name="email" onChange={handleChange} aria-required="true" />
-                     <label htmlFor="message">Message:</label>
-                     <textarea id="message" name="message" rows={5} cols={5} onChange={handleChange} aria-required="true" ></textarea>
-                     <ReCAPTCHA 
-                         ref={recaptchaRef}
-                         sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY}
-                         size="normal"
-                         id="recaptcha-google"
-                         onChange={() => setButtonDisabled(false)}
-                         />
-                     <ButtonWrap>
-                        <Button round="true" type="submit" disabled={buttonDisabled}>Submit</Button>          
-                     </ButtonWrap>
-                </FormWrap>
-             </form>
-         </EmailContent>
-     </EmailContainer>
-    </>
-  )
+                    </NetlifyForm>
+                </EmailContent>
+            </EmailContainer>
+        </>
+    )
 }
 
 export default Email
@@ -163,26 +133,9 @@ const FormWrap = styled.div`
      }
  }
 `
-const ButtonWrap=styled.div`
+const ButtonWrap = styled.div`
   display:flex;
   align-items:center;
   justify-content:center;
 `
-const Button=styled.button`
-background: ${({primary}) => (primary ? '#F26A2E': '#077BF1')};
- white-space:nowrap;
- padding: ${({ big }) => (big ? '16px 40px' : '10px 32px')};
- color:#fff;
- font-size: ${({ big }) => (big ? '20px' : '16px')};
- outline:none;
- border:none;
- min-width:100px;
- text-decoration:none;
- transition:0.3s !important;
- border-radius: ${({round}) => (round ? '50px': 'none')};
 
- &:hover {
-     background: ${({primary}) => (primary ? '#077BF1' : '#F26A2E')};
-     transform: translateY(-2px);
- }
-`
