7649fa00235cb09c719874e2008e75a9e6cdbb2a
Paul Contemplacion <paulbcon@gmail.com>
2023-11-20 05:41:47 -0500

    Add pictures

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import AniLink from "gatsby-plugin-transition-link/AniLink";
 import { StyledContainer, StyledHeading, StyledContent } from './StyledComponents';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const OurTeam = () => {
 
@@ -9,6 +10,7 @@ const OurTeam = () => {
       <StyledHeading>Our Team</StyledHeading>
       <StyledContent>
       <h4>Darius Cruz, CPA</h4>
+      <StaticImage src="../assets/images/dacruz.png" width={150} height={150} alt="Darius Cruz,CPA" />
        <p>
        Darius graduated with a Bachelor Degree in Accountancy from San Beda University-Manila, Philippines.  He has more than 20 years of professional experience that includes audit and assurance, accounting and bookkeeping, tax return preparation and consultation, tax compliance, and financial statement preparation.  Prior to moving to the United States, he was an Audit Senior Manager at Punongbayan & Araullo (now P&A Grant Thornton).  After moving to the United States, Darius acted as a Tax Manager at local CPA firms in Santa Monica and Camarillo.
        </p>
