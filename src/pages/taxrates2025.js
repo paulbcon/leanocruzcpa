fd2260bdef31fbf464075c480c43c67cd7f45e7b
Paul Contemplacion <paulbcon@gmail.com>
2025-12-14 19:48:41 -0500

    update pages

---
+++ src/pages/taxrates2025.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import TaxRates2025 from "../components/TaxRates2025"
+
+
+
+const TaxRates2025Page = () => (
+  <Layout>
+    <Seo title="Leano and Cruz, CPAs - 2025 Federal Income Tax Brackets (for taxes due in April 2026)" />
+   <TaxRates2025 />
+ </Layout>
+)
+
+export default TaxRates2025Page
+
---
+++ src/pages/taxrates2026.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import TaxRates2026 from "../components/TaxRates2026"
+
+
+const TaxRates2026Page = () => (
+  <Layout>
+    <Seo title="Leano and Cruz, CPAs - 2026 Federal Income Tax Brackets (for taxes due in April 2027)" />
+    <TaxRates2026 />
+ </Layout>
+)
+
+export default TaxRates2026Page
+
